129c9cb808a034a3316d163ee1699a59fbcf523d
neeraj-gs <gsneeraj05@gmail.com>
2023-07-21 11:14:10 +0530

    1.Creation , insertion Update Delete and Printing an array

--- 1.Arrays/Arr.java
+++ 1.Arrays/Arr.java
@@ -1,6 +1,27 @@
 //BASIC TO ADVANCED ARRAY QUESTIONS
+import java.util.*;
+public class Arr{
+    public static void printArray(int arr[]){
+        System.out.print("The Eleemtns of Array are: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        // int arr2[] = {1,2,5,6,8,}; //A way to create an array
+        System.out.print("Enter the Size of Array: ");
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the Elementa of Arrray: ");
+        for(int i=0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        printArray(arr);
 
-//1.
+    }
+}
 
 
 
