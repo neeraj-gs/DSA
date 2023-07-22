5db5e2dafc6188a4ae1b8870d86742a41378586c
neeraj-gs <gsneeraj05@gmail.com>
2023-07-22 11:28:49 +0530

    Started with 2nd Datastructure Matrix or 2d Arrays

---
+++ 2.Matrix/Matrix.java
@@ -0,0 +1,13 @@
+// /2D ARRAYS ALL BASIC TO INTERMIDIATE QUESTIONS
+import java.util.*;
+public class Matrix{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        // *******************************************************************************                Question 1 *                        ***************************************************************************************************
+        
+
+    }
+}
+
+
+
