47fe4326bfa738978aae63511369ea03ba9260bb
neeraj-gs <gsneeraj05@gmail.com>
2023-07-23 14:38:29 +0530

    7.String Compression using StringBuilder as Data Structure as Sb gives better time efficiency comapred to Strings

--- 3.Strings/Strings.java
+++ 3.Strings/Strings.java
@@ -68,19 +68,36 @@ public class Strings{
     //7.String COmpression
     public static String strCompress(String str){
         //using String
-        String newStr = "";
+        // String newStr = "";
+        // for(int i=0;i<str.length();i++){
+        //     Integer count = 1;
+        //     while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+        //         count++;
+        //         i++;
+        //     }
+        //     newStr+=str.charAt(i);
+        //     if(count>1){
+        //         newStr += count.toString();
+        //     }
+        // }
+        // return newStr;
+
+
+        //USing String Builder
+        StringBuilder sb = new StringBuilder("");
         for(int i=0;i<str.length();i++){
             Integer count = 1;
             while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
                 count++;
                 i++;
+
             }
-            newStr+=str.charAt(i);
+            sb.append(str.charAt(i));
             if(count>1){
-                newStr += count.toString();
+                sb.append(count.toString());
             }
         }
-        return newStr;
+        return sb.toString();
     }
 
 
