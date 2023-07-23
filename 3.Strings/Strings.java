e3ac555df973b32b0462c2824cd8bc331ceaed70
neeraj-gs <gsneeraj05@gmail.com>
2023-07-23 09:36:35 +0530

    2.Check if the Given String is Palindrome or Not

--- 3.Strings/Strings.java
+++ 3.Strings/Strings.java
@@ -2,6 +2,25 @@
 
 import java.util.*;
 public class Strings{
+
+    //2.Check if the strign is palindromw or not
+    public static boolean Palindrome(String str){
+        int s = 0;
+        int e = str.length()-1;
+        while(s<e){
+            if(str.charAt(s)!=str.charAt(e)){
+                System.out.println("Not a Palindrome");
+                return false;
+            }
+
+            s++;
+            e--;
+        }
+        System.out.println("IT is a Palindrome");
+        return true;
+    }
+
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         //**********************************************                          QUESTION 1                           ************************************************8*/
@@ -15,6 +34,9 @@ public class Strings{
         System.out.println("Length of String is: "+str.length()); //we also use concatenation in every rint str1+str2 = concatenation
         System.out.println("Charecter at Index 2 is: "+str.charAt(2));
 
+        //**********************************************                          QUESTION 2                           ************************************************8*/
+        //2.Palindrome or Not
+        Palindrome(str);
 
     }
 }
