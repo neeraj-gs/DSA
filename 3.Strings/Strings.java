a4610e5fe6726f2cf496a2dba33df24948921867
neeraj-gs <gsneeraj05@gmail.com>
2023-07-23 09:30:06 +0530

    1.Creation , Input , Output and In-Built String Functions

--- 3.Strings/Strings.java
+++ 3.Strings/Strings.java
@@ -4,6 +4,17 @@ import java.util.*;
 public class Strings{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        //**********************************************                          QUESTION 1                           ************************************************8*/
+        //1.CREATION , Input , Output and Built-in String Functions
+        System.out.print("ENter teh String: ");
+        String str = s.nextLine();
+        System.out.println("String entered is: "+str);
+        // String str = "STR";
+        // String str = new String("str"); //These 2 are other ways to Enter String
+
+        System.out.println("Length of String is: "+str.length()); //we also use concatenation in every rint str1+str2 = concatenation
+        System.out.println("Charecter at Index 2 is: "+str.charAt(2));
+
 
     }
 }
