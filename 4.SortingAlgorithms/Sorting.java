77f76984af82db7f0f56626136580b93b83ab045
neeraj-gs <gsneeraj05@gmail.com>
2023-07-25 10:46:03 +0530

    8.Count SOrt in Descendign Order

--- 4.SortingAlgorithms/Sorting.java
+++ 4.SortingAlgorithms/Sorting.java
@@ -92,6 +92,25 @@ public class Sorting{
 
     }
 
+    //8.Count SOrt in Descending Order
+    public static void countingSortDescending(int arr[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++) {
+            largest=Math.max(largest,arr[i]);
+        }
+        int count[] =new int[largest+1];
+        for(int i=0;i<arr.length;i++) {
+            count[arr[i]]++;
+        }
+        
+        int j=0;
+        for(int i=count.length-1;i>=0;i--){
+            while(count[i] >0) {
+                arr[j] =i;j++;count[i]--;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         int arr[] = {1,3,2,3,4,1,6,4,3};
@@ -116,6 +135,7 @@ public class Sorting{
             //find count of every distinct ele
             //calculate position of every elee in sorted array
             // countSort(arr);
+            countingSortDescending(arr);
             
     }
 }
