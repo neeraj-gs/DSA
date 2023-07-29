68b64a687f8bc981347f9725a182402f19032285
neeraj-gs <gsneeraj05@gmail.com>
2023-07-29 09:44:31 +0530

    1.Creation and 7 main Operations on Array list like add,get,remove,set,contains and size() OPeartions

--- 5.ArrayLists/ArrList.java
+++ 5.ArrayLists/ArrList.java
@@ -6,6 +6,41 @@ Array klist is a  calss in java
 import java.util.*;
 public class ArrList{
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(); //creation or initialisation of Array List
+        //<>  is used so that tht data type is stored only
+
+        //Operations in Array  List
+        //1.Add -- O(1) --
+        list.add(1); //adds eleemnt 1
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        //2.Get -- O(1) pass index and we get thte eleemnt
+        list.get(2); //-- we get 3 as output
+
+
+        //3.Remove por delete -- pass index and elee at that index is deelted
+        //O(n)
+        list.remove(1);
+
+        //4.Set element at index --O(n)
+        //pass index and the value to be stored at that index
+        list.set(2,10); //
+
+        //5.Contains element
+        //check if elee is present in array kist or not
+        list.contains(1); //we pass ele if presetn then true else false is returned
+
+        //6.Add at index
+        list.add(1,9); //takes index and value at that index and in that index that is added all elee are pushed back
+        //O(n)
+
+        //7.Size of an array list
+        list.size(); //gives the size of list
+        
+
         
     }
 }
