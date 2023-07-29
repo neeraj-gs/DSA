1fc9d3ee86a0bf85aea420c72c3f18b1c4eafd16
neeraj-gs <gsneeraj05@gmail.com>
2023-07-29 09:35:45 +0530

    Added Basic INtoduction or comments related ot Array List data structure

--- 5.ArrayLists/ArrList.java
+++ 5.ArrayLists/ArrList.java
@@ -1,4 +1,9 @@
 //ARRAY LISTS
+/*
+In BUilt data structure, that is linear and dynamic in size , Primitive data types cant be stored directly
+Array klist is a  calss in java
+*/
+import java.util.*;
 public class ArrList{
     public static void main(String[] args) {
         
