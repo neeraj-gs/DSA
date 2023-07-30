8cf7561c5044417c5a9d448eeb495d18c9bb1cc4
neeraj-gs <gsneeraj05@gmail.com>
2023-07-30 20:16:18 +0530

    2.Adding element to the last of the linked list

--- 6.LinkedList/LinkedList.java
+++ 6.LinkedList/LinkedList.java
@@ -18,21 +18,33 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    //1.Add Ele to the first in LL
     public void addFirst(int data){
         //Create new node
         Node newNode = new Node(data);
-        
+
         //if ll is emoty
         if(head==null){
             head= tail=newNode;
         }
-
-
-
         //New node next is head
         newNode.next=head; //link
         //head = newnode
         head=newNode;
+    }
+
+    //2.Add element to thte Last in LL
+    public void addLast(int data){
+        //Create new node
+        Node newNode = new Node(data);
+
+        //if ll is emoty
+        if(head==null){
+            head= tail=newNode;
+        }
+        tail.next=newNode;
+        tail=newNode;
+
     }
 
     public static void main(String[] args) {
@@ -41,8 +53,12 @@ public class LinkedList {
 
         //1.Add in Linked list -- add first or add last
         //a)addFirst
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
+
+        //b)addLast
+        ll.addLast(3);
+        ll.addLast(4);
         
 
     }
