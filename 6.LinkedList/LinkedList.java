993ec6a49ca99150bb3c5341af3eb12441c63e17
neeraj-gs <gsneeraj05@gmail.com>
2023-07-30 19:51:34 +0530

    Creation of Node

--- 6.LinkedList/LinkedList.java
+++ 6.LinkedList/LinkedList.java
@@ -6,5 +6,14 @@ Node has 2 parts 1.data 2.next node adress ends when next points to null
 
 */
 public class LinkedList {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
     
 }
