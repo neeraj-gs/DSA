567514b1bb5ddb0710ee2c2599c3d5d3049cf932
neeraj-gs <gsneeraj05@gmail.com>
2023-07-30 19:50:36 +0530

    Started with Linked list data structure added comments about the basic structure and information of linked list

---
+++ 6.LinkedList/LinkedList.java
@@ -0,0 +1,10 @@
+//LINKED LIST 
+/* 
+Linked k=lsit is like a chain if elements ,and the chani is called linkand each ele is called as node
+Node has 2 parts 1.data 2.next node adress ends when next points to null
+
+
+*/
+public class LinkedList {
+    
+}
