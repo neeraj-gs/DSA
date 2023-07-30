02736499138588a50a6ca800c7af56dc8288b1b7
neeraj-gs <gsneeraj05@gmail.com>
2023-07-30 20:07:01 +0530

    1.Add at the start or first of the Linked List

--- 6.LinkedList/LinkedList.java
+++ 6.LinkedList/LinkedList.java
@@ -18,12 +18,33 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data){
+        //Create new node
+        Node newNode = new Node(data);
+        
+        //if ll is emoty
+        if(head==null){
+            head= tail=newNode;
+        }
 
+
+
+        //New node next is head
+        newNode.next=head; //link
+        //head = newnode
+        head=newNode;
+    }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         //We will cerate methords and not write here
+
+        //1.Add in Linked list -- add first or add last
+        //a)addFirst
+        ll.addFirst(1);
+        ll.addFirst(2);
         
+
     }
     
 }
