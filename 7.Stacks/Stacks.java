6d3135972433db38302ac407afd3fb7c900cb57f
neeraj-gs <gsneeraj05@gmail.com>
2023-08-02 17:12:26 +0530

    3.IMplemetnation of Stack USing Linked LIst

--- 7.Stacks/Stacks.java
+++ 7.Stacks/Stacks.java
@@ -18,28 +18,81 @@ Stack - Last in First Out
 import java.util.ArrayList;
 
 public class Stacks{
-    static ArrayList<Integer> list = new ArrayList<>();
-    public static boolean isEmpty(){
-        return list.size()==0;
+    // static ArrayList<Integer> list = new ArrayList<>();
+    // public static boolean isEmpty(){
+    //     return list.size()==0;
+    // }
+
+    // //push
+    // public static void push(int data){
+    //     list.add(data);
+
+    // }
+
+    // //pop
+    // public static int pop(){
+    //     if(isEmpty()){
+    //         return -1;
+    //     }
+
+    //     int top = list.get(list.size()-1);
+    //     list.remove(list.size()-1);
+    //     return top;
+    // }
+
+    // //peek
+    // public static int peek(){
+    //     return top = list.get(list.size()-1);
+    // }
+
+    //2.IMplementation suing lInke dlist
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
     }
 
-    //push
-    public static void push(int data){
-        list.add(data);
+    static Node head = null;
 
+    //isEMpty
+    public static boolean isEmpty(){
+        return head==null;
+    }
+
+    //push 
+    public static void push(int data){
+        Node newNode = new Node(1);
+        if(isEmpty()){
+            head=newNode;
+        }
+        
+        newNode.next=head;
+        head=newNode;
     }
 
     //pop
     public static int pop(){
-        int top = list.get(list.size()-1);
-        list.remove(list.size()-1);
+        if(isEmpty()){
+            return -1;
+        }
+        int top = head.data;
+        head=head.next;
         return top;
+
     }
 
     //peek
     public static int peek(){
-        return top = list.get(list.size()-1);
+        if(isEmpty()){
+            return -1;
+        }
+        return head.data;
     }
+
+    
 
     public static void main(String[] args) {
         //1.Implement Stack Using Array Lists
@@ -53,5 +106,8 @@ public class Stacks{
             System.out.println(s.peek());
             // s.pop();
         }
+
+        //3.Implementation of Stack Using Linked List
+
     }
 }
