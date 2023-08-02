3855c5f35fea1f5b992d3f512a0ce5c4baf7ded9
neeraj-gs <gsneeraj05@gmail.com>
2023-08-02 16:59:14 +0530

    INformation related to Stack Data Structure

--- 7.Stacks/Stacks.java
+++ 7.Stacks/Stacks.java
@@ -1,6 +1,19 @@
 //Stacks Data Structure
+/*
+Stack - Last in First Out
+3 Operations --O(1)
+    1.Push - Adding data from top 
+    2.Pop  - Removing ele from top
+    3.Peek - Top Element of Stack
+
+    Implementation of Stacks -- Arrays,Array Lsit and Lined List
+    
 
 
+
+
+
+*/
 public class Stacks{
     public static void main(String[] args) {
         
