7f5b43fbb92fa4d00ad54c8792c0826ae22e6663
neeraj-gs <gsneeraj05@gmail.com>
2023-08-02 17:03:36 +0530

    1.IMplementation of Stacks Using Array Lists

--- 7.Stacks/Stacks.java
+++ 7.Stacks/Stacks.java
@@ -7,15 +7,47 @@ Stack - Last in First Out
     3.Peek - Top Element of Stack
 
     Implementation of Stacks -- Arrays,Array Lsit and Lined List
-    
+
 
 
 
 
 
 */
+
+import java.util.ArrayList;
+
 public class Stacks{
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static boolean isEmpty(){
+        return list.size()==0;
+    }
+
+    //push
+    public static void push(int data){
+        list.add(data);
+
+    }
+
+    //pop
+    public static int pop(){
+        int top = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
+
+    //peek
+    public static int peek(){
+        return top = list.get(list.size()-1);
+    }
+
     public static void main(String[] args) {
+        //1.Implement Stack Using Array Lists
+        Stacks s = new Stacks();
+        s.push(1);
+        s.push(2);
+        s.push(3);
         
+        //2.Printing in Stack
     }
 }
