a8fc140c186c0f3dd90c8410833ccd54cfc73025
neeraj-gs <gsneeraj05@gmail.com>
2023-08-07 15:31:37 +0530

    Added Comments or notes related to Queue Data Strucuture

--- 8_Queues/Queues.java
+++ 8_Queues/Queues.java
@@ -1,7 +1,21 @@
 //Queue Data Structure
 /*
 [Fifo]First in First out Data structure
+There are 2 pointers front and rear pointers pointing to start and end of queue
+exit - front pointer
+entry -rear pointer
 
+OPerations
+    1.Add - 0(1) -- Enqueue -- add to rear
+    2.Remove - O(1) - Dequeue -- remove elements from firs to front pointer
+    3.Peek - O(1) - Pesent element - front pointer
+
+IMplementation of Queues
+    1.Arrays -- fixed size(n) , remove-O(n) , we will not impleemtn queue using array
+        We can implement Circular Queue using Arrays
+        
+    2.Linked List 
+    3.Stacks
 
 
 */
