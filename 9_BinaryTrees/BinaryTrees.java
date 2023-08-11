71cbc772bc777f64f2ac1daef0b05169fc356c9c
neeraj-gs <gsneeraj05@gmail.com>
2023-08-11 18:40:20 +0530

    Added Notes and terminologies realted to Binary Trees

--- 9_BinaryTrees/BinaryTrees.java
+++ 9_BinaryTrees/BinaryTrees.java
@@ -1,4 +1,17 @@
 //BINARY TREES
+/* 
+Hierarchial Data Strucutre taht has a parent as root and connecting as branch and 
+BT has at max 2 Children
+
+Terminologies in Trees
+Levels - Data stored at each level from top to bottom from level 1 to ..
+Depth - level n - depth = n
+Sub Tree - Tre of a Specific node considering that node as a root[left and right sub tree]
+
+
+
+
+*/
 
 public class BinaryTrees {
     
