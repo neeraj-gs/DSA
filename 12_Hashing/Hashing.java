3e8e6e93af3aa0ca949aae83d98c132afbac8157
neeraj-gs <gsneeraj05@gmail.com>
2023-08-16 19:05:06 +0530

    Added basic information related to Hashing in java and their types

---
+++ 12_Hashing/Hashing.java
@@ -0,0 +1,23 @@
+/*
+Hasing in JAva
+    Technique where we convert one type of data to another
+    We use a hash function and then convert it
+        map and set are 2 data structures we create using hashing
+
+    Helps reduce the time complexity
+    Fast Execution -- Hsing in form of maps or sets
+
+    Maps - 
+    1.HashMap
+    2.Lined HashMap
+    3.TreeMap
+
+    Sets
+    1.HashSet
+    2.LinkedHashSet
+    3.TreeSet
+
+
+    
+
+*/
